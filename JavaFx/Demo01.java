0ec78d7c5a0c520356f5f09907ffc0858c7de594
SupunWellappili <supunchandana1814@gmail.com>
2024-01-10 01:57:10 +0530

    JavaFx intro

---
+++ JavaFx/Demo01.java
@@ -0,0 +1,15 @@
+package JavaFx;
+
+import javax.swing.*;
+
+public class Demo01 {
+    public static void main(String[] args) {
+
+        JFrame f1= new JFrame();
+        f1.setSize(300,300);
+        f1.setTitle("Calculator");
+        f1.setDefaultCloseOperation(2);
+        f1.setLocationRelativeTo(null);
+        f1.setVisible(true);
+    }
+}
