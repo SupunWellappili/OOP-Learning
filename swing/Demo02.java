da04f13de827a80b6ba80afada0b302045abad6a
SupunWellappili <supunchandana1814@gmail.com>
2024-01-10 09:04:36 +0530

    swing intro -02

--- swing/Demo02.java
+++ swing/Demo02.java
@@ -1,17 +1,22 @@
 package swing;
 
+import javax.swing.*;
+
 public class Demo02 {
     public static void main(String[] args) {
-        int length;
+        /*int length;
         int height;
-        int width;
+        int width;*/
+        JFrame f1 = new JFrame();
 
-        length = 12;
+        /*length = 12;
         height = 5;
-        width = 4;
+        width = 4;*/
+        f1.setSize(12,5,4);
 
-        int volume = length * height * width;
+       // int volume = length * height * width;
         System.out.println("Box Volume Is : " + volume);
+        printVolume();
 
     }
 }
