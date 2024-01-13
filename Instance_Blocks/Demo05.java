cf363fc6716fcd65c163f6a189116adf0aa15251
SupunWellappili <supunchandana1814@gmail.com>
2024-01-13 23:26:48 +0530

    Null-point-Exception

---
+++ Instance_Blocks/Demo05.java
@@ -0,0 +1,18 @@
+package Instance_Blocks;
+
+
+class Fish{
+    public void mA(){
+        System.out.println("mA()");
+    }
+
+}
+public class Demo05 {
+    public static void main(String[] args) {
+        Fish f1 = null;//Exception handling awe neththee call krnne object 1ken nowana nisaa.call krnne Test aken anam template aken
+        f1.mA();//Legal
+    }
+}
+
+
+//Compile time Run we..Runtime Error(Null point Exception)
