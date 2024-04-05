6c79cb638184abea5f4af8484335dde0c44c7736
SupunWellappili <supunchandana1814@gmail.com>
2024-04-05 23:58:08 +0530

    Outpur Priview

---
+++ Inheritance/Demo01.java
@@ -0,0 +1,16 @@
+package Inheritance;
+import javax.swing.*;
+import java.util.Calendar;
+
+class  Calculator extends JFrame {
+
+}
+
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.setVisible(true);
+        c1.setSize(500,500);
+    }
+}
