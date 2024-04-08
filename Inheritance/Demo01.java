cff51bc169555e6fd341c361d834f3bceabb5def
SupunWellappili <supunchandana1814@gmail.com>
2024-04-08 11:34:35 +0530

    Inheritance 01

--- Inheritance/Demo01.java
+++ Inheritance/Demo01.java
@@ -11,6 +11,11 @@ public class Demo01 {
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
         c1.setVisible(true);
+        c1.setTitle("Calculator Tittle");
         c1.setSize(500,500);
+        c1.hashCode();
+        c1.toString();
+        c1.notify();
+
     }
 }
