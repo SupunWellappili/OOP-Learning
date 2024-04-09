efa30ab7501891b8e0336117c5583d972017a61b
SupunWellappili <supunchandana1814@gmail.com>
2024-04-09 23:02:36 +0530

    Update

--- Polymorphism/Demo01.java
+++ Polymorphism/Demo01.java
@@ -1,21 +1,31 @@
 package Polymorphism;
 
 
-class Car {
+class Vehicle {
     public void park() {
-        System.out.println("Car Parking");
+        System.out.println("Vehicle Parking(00.00)");
+    }
+
+    public void pay() {
+        System.out.println("Vehicle Payment");
     }
 }
 
-class Van {
+class Car extends Vehicle {
     public void park() {
-        System.out.println("Van Parking");
+        System.out.println("Car Parking(50)");
     }
 }
 
-class Bus {
+class Van extends Vehicle {
     public void park() {
-        System.out.println("Bus Parking");
+        System.out.println("Van Parking(50)");
+    }
+}
+
+class Bus extends Vehicle {
+    public void park() {
+        System.out.println("Bus Parking(100)");
     }
 
     public void pay() {
@@ -24,8 +34,24 @@ class Bus {
 }
 
 
-class Demo01{
+class Demo01 {
     public static void main(String[] args) {
+        Vehicle v1 = new Car();
+        v1.park();
+        v1.park();
+
+        System.out.println("==========");
+
+        v1 = new Van();
+        v1.park();
+        v1.pay();
+
+        System.out.println("==========");
+
+        v1 = new Bus();
+        v1.park();
+        v1.pay();
+
 
     }
 }
