56ccf28e51cdd07aad8d2db45c6e3018586e8b3c
SupunWellappili <supunchandana1814@gmail.com>
2024-04-09 22:57:42 +0530

    Polymorphism intro

---
+++ Polymorphism/Demo01.java
@@ -0,0 +1,31 @@
+package Polymorphism;
+
+
+class Car {
+    public void park() {
+        System.out.println("Car Parking");
+    }
+}
+
+class Van {
+    public void park() {
+        System.out.println("Van Parking");
+    }
+}
+
+class Bus {
+    public void park() {
+        System.out.println("Bus Parking");
+    }
+
+    public void pay() {
+        System.out.println("Pay Payment");
+    }
+}
+
+
+class Demo01{
+    public static void main(String[] args) {
+
+    }
+}
