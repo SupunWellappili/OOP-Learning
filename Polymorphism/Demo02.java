adec77578a72aa438579d9998edce7b9fa7146bc
SupunWellappili <supunchandana1814@gmail.com>
2024-04-09 23:26:16 +0530

    Polymorphism Intro

---
+++ Polymorphism/Demo02.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+
+  class IPhone1 { //Concrete Class
+    String model = "IPhone1";
+
+    public void call() { //Concrete Method
+        System.out.println("Calling");
+    }
+
+    // public void sendMessage(); //Abstract Method (Implementation akak nee (Body akak na))
+}
+
+public class Demo02 {
+    public static void main(String[] args) {
+
+    }
+}
