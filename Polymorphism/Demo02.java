a4e273b79c7facc87ab5612c711763a2a062e2d0
SupunWellappili <supunchandana1814@gmail.com>
2024-04-10 07:11:38 +0530

    Add To Abstract KeyWord

--- Polymorphism/Demo02.java
+++ Polymorphism/Demo02.java
@@ -1,14 +1,14 @@
 package Polymorphism;
 
 
-  class IPhone1 { //Concrete Class
+abstract class IPhone1 { //Concrete Class
     String model = "IPhone1";
 
     public void call() { //Concrete Method
         System.out.println("Calling");
     }
 
-    // public void sendMessage(); //Abstract Method (Implementation akak nee (Body akak na))
+    abstract public void sendMessage(); //Abstract Method (Implementation akak nee (Body akak na))
 }
 
 public class Demo02 {
