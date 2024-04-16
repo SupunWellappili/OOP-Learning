973cfb023f8f40550e74e6db3360fd47b33652ad
SupunWellappili <supunchandana1814@gmail.com>
2024-04-16 23:15:29 +0530

    Try_Catch Enable

--- Exception/Demo02.java
+++ Exception/Demo02.java
@@ -4,7 +4,11 @@ public class Demo02 {
     public static void main(String[] args) {
         System.out.println("Start Main");
         int x = 10;
-        int y = x / 0;
+        try {
+            int y = x / 0;
+        }catch (Exception e){
+            System.out.println(e);
+        }
         System.out.println(x);
         System.out.println("End Main");
     }
