e7638c95c6567853d6db2de451313165ba794597
SupunWellappili <supunchandana1814@gmail.com>
2024-04-16 16:00:16 +0530

    Garbage Collector

--- Garbage_Collector/Demo01.java
+++ Garbage_Collector/Demo01.java
@@ -3,16 +3,18 @@ package Garbage_Collector;
 class MyClass {
     long[] xr = new long[100000];
 
+    public void finalize(){ //GC
+        System.out.println("Deleted....");
+    }
 }
 
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Start Main");
-        MyClass[] cr = new MyClass[10000];
 
         for (int i = 0; i < 10000; i++) {
             System.out.println("i: " + i);
-           cr[i]= new MyClass();
+           new MyClass();
         }
         System.out.println("End Main");
 
